16196fcce676e4872489571f20e2d42c941f59eb
git-spiki <publicspiki@gmail.com>
2024-11-27 18:16:31 +1000

    Add files via upload

---
+++ WpfApp3/WpfApp3/Window1.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        private List<string> ImagesStore;
+
+        private void UpdateImageRender()
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\icons\memes";
+            if (Directory.Exists(path))
+            {
+                ImagesStore = new List<string>(Directory.GetFiles(path, "*.jpg"));
+                ImagesStore.AddRange(Directory.GetFiles(path, "*.png"));
+                ImagesStore.AddRange(Directory.GetFiles(path, "*.jpeg"));
+
+                int index = 0;
+                if (RadioImage1.IsChecked == true)
+                {
+                    index = 0;
+                }
+                else if (RadioImage2.IsChecked == true)
+                {
+                    index = 1;
+                }
+                else if (RadioImage3.IsChecked == true)
+                {
+                    index = 2;
+                }
+
+                if (ImagesStore.Count < index+1)
+                {
+                    MessageBox.Show("Индекс выходит за границы листа");
+                    return;
+                }
+
+                RenderImageLabel.Source = new BitmapImage(new Uri(ImagesStore[index]));
+            }
+        }
+
+        public Window1()
+        {
+            InitializeComponent();
+            UpdateImageRender();
+         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow next = new MainWindow();
+            next.Show();
+            this.Close();
+        }
+
+        private void RadioImage1_Checked(object sender, RoutedEventArgs e)
+        {
+            UpdateImageRender();
+        }
+
+        private void RadioImage2_Checked(object sender, RoutedEventArgs e)
+        {
+            UpdateImageRender();
+        }
+
+        private void RadioImage3_Checked(object sender, RoutedEventArgs e)
+        {
+            UpdateImageRender();
+        }
+    }
+}
---
+++ WpfApp3/WpfApp3/Window2.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Логика взаимодействия для Window2.xaml
+    /// </summary>
+    public partial class Window2 : Window
+    {
+        public Window2()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new Microsoft.Win32.OpenFileDialog();
+            //dialog.FileName = "Document";
+            //dialog.DefaultExt = ".txt";
+            dialog.Filter = "Image|*.png;*.jpg;*.jpeg";
+            
+            bool? result = dialog.ShowDialog();
+            
+            if (result == true)
+            {
+                string filename = dialog.FileName;
+                ShowerImage.Source = new BitmapImage(new Uri(filename));
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MainWindow w = new MainWindow();
+            w.Show();
+            this.Close();
+        }
+    }
+}
